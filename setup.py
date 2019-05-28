4d60605b1b675834c1224ccd4ebd6e7827ad4134
Ruturaj Vaidya <ruturajkvaidya@ku.edu>
2019-05-28 11:50:04 -0500

    restructure

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='securepack',
     version='0.0.1',
-    entry_points={
-        'console_scripts': [
-            'securepack=securepack:securepack'
-        ]
-    }
+    author="Ruturaj Kiran Vaidya",
+    author_email="ruturajkvaidya@gmail.com",
+    description="Securepack helps securing packages from typosquatting attack
+",
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ruturaj4/securepack",
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+       'fuzzyset>=0.0.18',
+    ]
 )
